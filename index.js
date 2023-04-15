0835736f650bcbfcbcc634b48589792ec316ca79
tarikul-islam <tariikul12@gmail.com>
2023-04-15 11:18:53 +0600

    server create

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/',(req, res)=>{
+    res.send('Hello from my first ever server')
+})
+app.get('/data',(req,res) =>{
+    res.send('my Project Coming soon')
+})
+
+app.listen(port, () =>{
+    console.log(`My first server is running on port: ${port}`)
+})
